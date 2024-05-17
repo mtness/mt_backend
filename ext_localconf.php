ee73a605effe539c3ee3d80ddf0a7ba25e3f8896
mtness <markus@timtner.com>
2024-05-17 15:26:10 +0200

    [FIX] removed unnecessary condition

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,6 @@
 <?php
 defined('TYPO3') || die('Access denied.');
 
-if (TYPO3 === 'BE') {
-
-	// Hook into the page module
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/db_layout.php']['drawHeaderHook']['mt_backend'] =
-		\MarkusTimtner\MtBackend\Hooks\PageHook::class . '->render';
-}
+// Hook into the page module
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/db_layout.php']['drawHeaderHook']['mt_backend'] =
+	\MarkusTimtner\MtBackend\Hooks\PageHook::class . '->render';
