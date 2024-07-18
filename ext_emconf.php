b6ffac223f6453753ff482979f71224a2dcb02bb
mtness <markus@timtner.com>
2024-07-18 16:46:25 +0200

    [TASK] bump Version for TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
   'createDirs' => '',
   'constraints' => [
     'depends' => [
-      'typo3' => '10.0.0-11.5.99',
+      'typo3' => '10.0.0-12.5.99',
 	],
     'conflicts' => [],
     'suggests' => [],
