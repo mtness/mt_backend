cbf2bb02294430d9c4de29a57fca68c2d08aeef2
mtness <markus@timtner.com>
2024-07-19 13:40:23 +0200

    [FIX] cleanup emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,26 +1,14 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "mt_backend".
- *
- * Auto generated 21-04-2016 12:34
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = [
-  'title' => 'MT BACKEND.',
+  'title' => 'MT BACKEND',
   'description' => 'This TYPO3 Extension provides a preview of page properties in the page module, and a some css touchup.',
   'category' => 'be',
+  'version' => '12.0.2',
+  'state' => 'stable',
   'author' => 'Markus Timtner',
   'author_email' => 'markus@timtner.tech',
   'author_company' => '',
-  'version' => '12.0.1',
-  'state' => 'stable',
-  'uploadfolder' => false,
-  'createDirs' => '',
   'constraints' => [
     'depends' => [
       'typo3' => '10.0.0-12.5.99',
