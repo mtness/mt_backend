7fc78f02dc4bcaa1bbcee8df15a03b25e9972af7
Markus Timtner <markus@timtner.com>
2025-01-30 13:14:19 +0100

    added TYPO3 v13 compatibility

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,11 +5,12 @@ defined('TYPO3') || die('Access denied.');
 $versionInformation = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
 if ($versionInformation->getMajorVersion() > 12) {
 
+} else {
+
 	// Hook into the page module
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/db_layout.php']['drawHeaderHook']['mt_backend'] =
 		\MarkusTimtner\MtBackend\Hooks\PageHook::class . '->render';
-} else {
-
-	$GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['mt_backend'] = 'EXT:mt_backend/Resources/Public/Css/MyBeTouchup.css';
 
 }
+
+$GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['mt_backend'] = 'EXT:mt_backend/Resources/Public/Css/MyBeTouchup.css';
